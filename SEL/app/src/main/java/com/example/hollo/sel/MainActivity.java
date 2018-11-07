fc88f4404d26716829a903c66668231d18b80bee
jrojas10 <jrojasbf@gmail.com>
2018-11-07 11:52:56 -0800

    added firebase

--- SEL/app/src/main/java/com/example/hollo/sel/MainActivity.java
+++ SEL/app/src/main/java/com/example/hollo/sel/MainActivity.java
@@ -3,10 +3,21 @@ package com.example.hollo.sel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Access a Cloud Firestore instance from your Activity
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
---
+++ SEL/app/src/main/java/com/example/hollo/sel/models/User.java
@@ -0,0 +1,19 @@
+package com.example.hollo.sel.models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+
+    public String username;
+    public String email;
+
+    public User() {
+
+    }
+    public User(String username, String email){
+        this.username = username;
+        this.email = email;
+    }
+}
+
