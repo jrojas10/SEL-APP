f88a894b0c8f96b5d3d48f2d1de436ecaaacec14
Truong, Loc <Loc.Truong@disney.com>
2018-11-09 13:26:01 -0800

    added simple activity to main activity to add a user

--- SEL/app/src/main/java/com/example/hollo/sel/models/User.java
+++ SEL/app/src/main/java/com/example/hollo/sel/models/User.java
@@ -1,19 +1,35 @@
 package com.example.hollo.sel.models;
 
-import com.google.firebase.database.IgnoreExtraProperties;
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
-@IgnoreExtraProperties
+/*
+Add these annotations
+@Entity(tableName = "word_table")
+   - Each @Entity class represents an entity in a table. Annotate your class declaration to indicate that it's an entity. Specify the name of the table if you want it to be different from the name of the class.
+@PrimaryKey
+   - Every entity needs a primary key. To keep things simple, each word acts as its own primary key.
+@NonNull
+   - Denotes that a parameter, field, or method return value can never be null.
+@ColumnInfo(name = "word")
+   - Specify the name of the column in the table if you want it to be different from the name of the member variable.
+   - Every field that's stored in the database needs to be either public or have a "getter" method. This sample provides a getWord() method.
+* */
+
+@Entity(tableName = "user_table")
 public class User {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "user")
+    private String mUser;
 
-    public String username;
-    public String email;
-
-    public User() {
-
+    public User(@NonNull String user) {
+        this.mUser = user;
     }
-    public User(String username, String email){
-        this.username = username;
-        this.email = email;
+
+    public String getUser() {
+        return this.mUser;
     }
 }
-
---
+++ SEL/app/src/main/java/com/example/hollo/sel/models/UserRepository.java
@@ -0,0 +1,40 @@
+package com.example.hollo.sel.models;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class UserRepository {
+    private UserDao mUserDao;
+    private LiveData<List<User>> mAllUsers;
+
+    public UserRepository(Application application) {
+        UserRoomDatabase db = UserRoomDatabase.getDatabase(application.getApplicationContext());
+        mUserDao = db.userDao();
+        mAllUsers = mUserDao.getAllUsers();
+    }
+
+    LiveData<List<User>> getAllUsers() {
+        return mAllUsers;
+    }
+
+    public void insert(User user) {
+        new insertAsyncTask(mUserDao).execute(user);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<User, Void, Void> {
+        private UserDao mAsyncTaskDao;
+
+        insertAsyncTask(UserDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final User... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
